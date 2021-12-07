cd3010eb4a85f92a26253e07527dbf120c7e0f9d
chubumis <74563547+chubumis@users.noreply.github.com>
2021-12-06 22:30:17 -1200

    Music main layout
    Added all the different areas where the beat will be clicked so user can click that part of the sequencer

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import random
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.pagelayout import PageLayout
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+Window.clearcolor = (0.5,0,1,1)
 
-
+'''    canvas.before:
+        Color:
+            rgba: (0.5,0,0.8,1)
+        Rectangle:
+            pos: self.pos
+            size: self.size
+            '''
 
 class BoxLayoutExample(BoxLayout):
     pass
@@ -16,7 +28,12 @@ class BoxLayoutExample(BoxLayout):
         b2 = Button(text="B")
         self.add_widget(b1)
         self.add_widget(b2)
+        
 '''
+
+class BoxLayoutSequencer(FloatLayout):
+    pass
+
 class MainWidget(Widget):
     pass
 
@@ -24,3 +41,4 @@ class TheLabApp(App):
     pass
 
 TheLabApp().run()
+
