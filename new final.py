81abf98578771d174fbf7622d4a3330d860dde3e
chubumis <74563547+chubumis@users.noreply.github.com>
2021-12-13 09:50:00 -0500

    update
    all button work properly for bass

--- new final.py
+++ new final.py
@@ -1,5 +1,5 @@
 import pygame, sys, random, time
-from time import time
+
 pygame.init()
 display_w = 1400
 display_h = 800
@@ -23,6 +23,8 @@ tempPurp = [(255,127,80),(255,127,80),(255,127,80),(255,127,80),(255,127,80),(25
 magenta = 255,0,255
 width = 62
 height = 80
+timer = 0
+
 
 
 
@@ -36,6 +38,10 @@ gameState = 'game'
 
 
 def musicRooms():
+    global timer
+    timer += 1
+    print(timer)
+    global tempPurp
     global purple
     global purple1
     global purple2
@@ -43,6 +49,7 @@ def musicRooms():
     global purple4
     global clicked
     global i
+    global mouse_pos
 
 
 
@@ -52,6 +59,7 @@ def musicRooms():
     bassR1 = [1,2,3,4]
     mousePos = pygame.mouse.get_pos()
     if gameState == 'game':
+        mouse_pos = pygame.mouse.get_pos()
         screen.fill(black)
         tempoBar_1 = pygame.draw.rect(screen, tempPurp[0], pygame.Rect(222,150, 62,25))
         tempoBar_2 = pygame.draw.rect(screen, tempPurp[1], pygame.Rect(288,150, 62,25))
@@ -69,6 +77,58 @@ def musicRooms():
         tempoBar_14 = pygame.draw.rect(screen, tempPurp[13], pygame.Rect(1110,150, 62,25))
         tempoBar_15 = pygame.draw.rect(screen, tempPurp[14], pygame.Rect(1176,150, 62,25))
         tempoBar_16 = pygame.draw.rect(screen, tempPurp[15], pygame.Rect(1242,150, 62,25))
+
+        if timer > 100:
+            tempPurp[0] = 230, 0, 0
+        if timer > 200:
+            tempPurp[0] = 255,127,80
+            tempPurp[1] = 230, 0, 0
+        if timer > 300:
+            tempPurp[1] = 255,127,80
+            tempPurp[2] = 230, 0, 0
+        if timer > 400:
+            tempPurp[2] = 255,127,80
+            tempPurp[3] = 230, 0, 0
+        if timer > 500:
+            tempPurp[3] = 255,127,80
+            tempPurp[4] = 230, 0, 0
+        if timer > 600:
+            tempPurp[4] = 255,127,80
+            tempPurp[5] = 230, 0, 0
+        if timer > 700:
+            tempPurp[5] = 255,127,80
+            tempPurp[6] = 230, 0, 0
+        if timer > 800:
+            tempPurp[6] = 255,127,80
+            tempPurp[7] = 230, 0, 0
+        if timer > 900:
+            tempPurp[7] = 255,127,80
+            tempPurp[8] = 230, 0, 0
+        if timer > 1000:
+            tempPurp[8] = 255,127,80
+            tempPurp[9] = 230, 0, 0
+        if timer > 1200:
+            tempPurp[9] = 255,127,80
+            tempPurp[10] = 230, 0, 0
+        if timer > 1300:
+            tempPurp[10] = 255,127,80
+            tempPurp[11] = 230, 0, 0
+        if timer > 1400:
+            tempPurp[11] = 255,127,80
+            tempPurp[12] = 230, 0, 0
+        if timer > 1500:
+            tempPurp[12] = 255,127,80
+            tempPurp[13] = 230, 0, 0
+        if timer > 1600:
+            tempPurp[13] = 255,127,80
+            tempPurp[14] = 230, 0, 0
+        if timer > 1700:
+            tempPurp[14] = 255,127,80
+            tempPurp[15] = 230, 0, 0
+        if timer > 1790:
+            timer = 100
+
+
 
         bassBar = pygame.draw.rect(screen, purple, pygame.Rect(30,230, 170,80))
         bass4 = [(x,y,width,height),(x+66,y,width,height),(x+132,y,width,height),(x+198,y,width,height)]
@@ -101,115 +161,157 @@ def musicRooms():
 
 
         #if event.type == pygame.MOUSEMOTION:
-            #time.sleep(0.2)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
+            ##
+        if pygame.mouse.get_pressed()[0]:
+
             if mouse_pos[0] in list(range(220, 280)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purple1 == (159, 43, 104):
                     purple1 = 0, 43, 104
-                elif purple1 == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220, 280)) and mouse_pos[1] in list(range(230, 310)):
+                if purple1 == (0, 43, 104):
                     purple1 = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+66, 280+66)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purple2 == (159, 43, 104):
                     purple2 = 0, 43, 104
-                elif purple2 == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+66, 280+66)) and mouse_pos[1] in list(range(230, 310)):
+                if purple2 == (0, 43, 104):
                     purple2 = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+132, 280+132)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purple3 == (159, 43, 104):
                     purple3 = 0, 43, 104
-                elif purple3 == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+132, 280+132)) and mouse_pos[1] in list(range(230, 310)):
+                if purple3 == (0, 43, 104):
                     purple3 = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+198, 280+198)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purple4 == (159, 43, 104):
                     purple4 = 0, 43, 104
-                elif purple4 == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+198, 280+198)) and mouse_pos[1] in list(range(230, 310)):
+                if purple4 == (0, 43, 104):
                     purple4 = 159, 43, 104
 
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+274, 280+274)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL[0] == (159, 43, 104):
                     purpleL[0] = 0, 43, 104
-                elif purpleL[0] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+274, 280+274)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL[0] == (0, 43, 104):
                     purpleL[0] = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+340, 280+340)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL[1] == (159, 43, 104):
                     purpleL[1] = 0, 43, 104
-                elif purpleL[1] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+340, 280+340)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL[1] == (0, 43, 104):
                     purpleL[1] = 159, 43, 104
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+406, 280+406)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL[2] == (159, 43, 104):
                     purpleL[2] = 0, 43, 104
-                elif purpleL[2] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+406, 280+406)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL[2] == (0, 43, 104):
                     purpleL[2] = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+472, 280+472)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL[3] == (159, 43, 104):
                     purpleL[3] = 0, 43, 104
-                elif purpleL[3] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+472, 280+472)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL[3] == (0, 43, 104):
                     purpleL[3] = 159, 43, 104
 
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+548, 280+548)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL1[0] == (159, 43, 104):
                     purpleL1[0] = 0, 43, 104
-                elif purpleL1[0] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+548, 280+548)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL1[0] == (0, 43, 104):
                     purpleL1[0] = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+614, 280+614)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL1[1] == (159, 43, 104):
                     purpleL1[1] = 0, 43, 104
-                elif purpleL1[1] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+614, 280+614)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL1[1] == (0, 43, 104):
                     purpleL1[1] = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+680, 280+680)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL1[2] == (159, 43, 104):
                     purpleL1[2] = 0, 43, 104
-                elif purpleL1[2] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+680, 280+680)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL1[2] == (0, 43, 104):
                     purpleL1[2] = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+746, 280+746)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL1[3] == (159, 43, 104):
                     purpleL1[3] = 0, 43, 104
-                elif purpleL1[3] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+746, 280+746)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL1[3] == (0, 43, 104):
                     purpleL1[3] = 159, 43, 104
 
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+822, 280+822)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL2[0] == (159, 43, 104):
                     purpleL2[0] = 0, 43, 104
-                elif purpleL2[0] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+822, 280+822)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL2[0] == (0, 43, 104):
                     purpleL2[0] = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+888, 280+888)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL2[1] == (159, 43, 104):
                     purpleL2[1] = 0, 43, 104
-                elif purpleL2[1] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+888, 280+888)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL2[1] == (0, 43, 104):
                     purpleL2[1] = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+954, 280+954)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL2[2] == (159, 43, 104):
                     purpleL2[2] = 0, 43, 104
-                elif purpleL2[2] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+954, 280+954)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL2[2] == (0, 43, 104):
                     purpleL2[2] = 159, 43, 104
+
+        if pygame.mouse.get_pressed()[0]:
             if mouse_pos[0] in list(range(220+1020, 280+1020)) and mouse_pos[1] in list(range(230, 310)):
-                time.sleep(0.2)
                 if purpleL2[3] == (159, 43, 104):
                     purpleL2[3] = 0, 43, 104
-                elif purpleL2[3] == (0, 43, 104):
+        if pygame.mouse.get_pressed()[2]:
+            if mouse_pos[0] in list(range(220+954, 280+954)) and mouse_pos[1] in list(range(230, 310)):
+                if purpleL2[3] == (0, 43, 104):
                     purpleL2[3] = 159, 43, 104
 
 
 
         pygame.display.flip()
-prev = time()
+
 while True:
-    timer()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
